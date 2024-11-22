cdcf8fed87b55a19c5cdfa0d3abc0183729f9cdc
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-11-22 11:04:45 +0530

    Add files via upload

---
+++ src/app/shared/search-filter/search-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-search-filter',
+  templateUrl: './search-filter.component.html',
+  styleUrls: ['./search-filter.component.scss']
+})
+export class SearchFilterComponent {
+  @Output() search = new EventEmitter<string>();
+
+  onSearch(event: any): void {
+    const query = event.target.value.trim();
+    this.search.emit(query);
+  }
+}
